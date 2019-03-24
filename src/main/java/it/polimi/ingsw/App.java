91fc5a36842493507156c8a451a28c84cbcbb5ae
xX-1337-Xx <francesco3.dotti@mail.polimi.it>
2019-03-24 20:46:12 +0100

    edit pom.xml and readme

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -2,13 +2,10 @@ package it.polimi.ingsw;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         int a = 1;
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
