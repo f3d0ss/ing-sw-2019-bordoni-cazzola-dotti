4c8f7d4401f42ba2b6450a99a84d654add945126
supernivem <48649510+supernivem@users.noreply.github.com>
2019-04-09 15:18:52 +0200

    Match&PowerUpDeck

--- src/main/java/it/polimi/ingsw/model/Match.java
+++ src/main/java/it/polimi/ingsw/model/Match.java
@@ -4,16 +4,23 @@ import java.util.ArrayList;
 
 public class Match {
 
-    private ArrayList<PlayerId> killShootTrack = new ArrayList<PlayerId>();
-//    private PowerUpDeck currentPowerUpDeck;
-//    private PowerUpDeck usedPowerUpDeck;
+//    private ArrayList<PlayerId> killShootTrack;
+    private PowerUpDeck currentPowerUpDeck;
+    private PowerUpDeck usedPowerUpDeck;
 //    private AmmoTileDeck currentAmmoTileDeck;
 //    private AmmoTileDeck usedAmmoTileDeck;
 //    private WeaponDeck currentWeaponDeck;
-    private ArrayList<Player> deadPlayers = new ArrayList<Player>();
-    private int deathsCounter;
-    private PlayerId currentPlayer;
-    private ArrayList<Player> currentPlayers = new ArrayList<Player>();
+//    private ArrayList<Player> deadPlayers;
+//    private int deathsCounter;
+//    private PlayerId currentPlayer;
+    private ArrayList<Player> currentPlayers;
+    private GameBoard board;
+
+    public Match(PowerUpDeck currentPowerUpDeck, PowerUpDeck usedPowerUpDeck, ArrayList<Player> currentPlayers) {
+        this.currentPowerUpDeck = currentPowerUpDeck;
+        this.usedPowerUpDeck = usedPowerUpDeck;
+        this.currentPlayers = currentPlayers;
+    }
 
     public Player getPlayer(PlayerId id){
         for(Player tmp : currentPlayers)
@@ -23,7 +30,37 @@ public class Match {
         return null;
     }
 
+    public GameBoard getBoard() {
+        return board;
+    }
+
     public void addPlayer(Player player){
         currentPlayers.add(player);
+    }
+
+    public AmmoTile drawAmmoTileCard(){
+        AmmoTile tmp;
+        AmmoTile emptyDeck;
+        tmp = currentAmmoTileDeck.drawAmmoTile();
+        if (tmp == null){
+            emptyDeck = currentAmmoTileDeck;
+            currentAmmoTileDeck = usedAmmoTileDeck;
+            usedAmmoTileDeck = emptyDeck;
+            tmp = currentAmmoTileDeck.drawAmmoTile();
+        }
+        return tmp;
+    }
+
+    public PowerUp drawPowerUpCard(){
+        PowerUp tmp;
+        PowerUpDeck emptyDeck;
+        tmp = currentPowerUpDeck.drawPowerUp();
+        if (tmp == null){
+            emptyDeck = currentPowerUpDeck;
+            currentPowerUpDeck = usedPowerUpDeck;
+            usedPowerUpDeck = emptyDeck;
+            tmp = currentPowerUpDeck.drawPowerUp();
+        }
+        return tmp;
     }
 }
