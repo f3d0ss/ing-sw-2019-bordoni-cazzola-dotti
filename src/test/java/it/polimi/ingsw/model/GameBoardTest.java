9a190d33927db960097854c5e825a7567c3b1112
supernivem <48649510+supernivem@users.noreply.github.com>
2019-04-16 00:13:34 +0200

    GameBoard new methods + testing

--- src/test/java/it/polimi/ingsw/model/GameBoardTest.java
+++ src/test/java/it/polimi/ingsw/model/GameBoardTest.java
@@ -10,6 +10,7 @@ public class GameBoardTest {
 
     private final static int ROWS = 3;
     private final static int COLUMNS = 4;
+    private final static int INF = 4;
 
     //test the correct search of visible squares - only some cases are tested
 
@@ -22,7 +23,7 @@ public class GameBoardTest {
             for (int j = 0; j < COLUMNS; j++) {
                 square = board.getSquare(i, j);
                 if (square != null) {
-                    list = board.getVisibleSquares(square);
+                    list = board.getVisibleSquares(square, INF, 0, false);
                     if (i == 0 && j == 0 || i == 1 && j == 0 || i == 0 && j == 2) {
                         assertEquals(list.contains(board.getSquare(0, 0)), true);
                         assertEquals(list.contains(board.getSquare(0, 1)), true);
@@ -124,20 +125,72 @@ public class GameBoardTest {
     public void testGetVisibleTarget() {
         Match match = new Match();
         Square square = match.getBoard().getSquare(0, 0);
-        ArrayList<Player> competitors;
+        ArrayList<Square> competitors;
         Player one = new Player(match, null, null, null);
         one.respawn(Color.RED);
         one.move(CardinalDirection.NORTH);
+        one.getPosition().addPlayer(one);
         Player two = new Player(match, null, null, null);
         two.respawn(Color.RED);
+        two.getPosition().addPlayer(two);
         Player three = new Player(match, null, null, null);
         three.respawn(Color.BLUE);
+        three.getPosition().addPlayer(three);
         Player four = new Player(match, null, null, null);
         four.respawn(Color.YELLOW);
-        competitors = match.getBoard().getVisibleTarget(square);
-        assertEquals(competitors.contains(one), true);
-        assertEquals(competitors.contains(two), true);
-        assertEquals(competitors.contains(three), true);
-        assertEquals(competitors.contains(four), false);
+        four.getPosition().addPlayer(four);
+        competitors = match.getBoard().getVisibleSquares(square, INF, 0, true);
+        assertEquals(competitors.contains(one.getPosition()), true);
+        assertEquals(competitors.contains(two.getPosition()), true);
+        assertEquals(competitors.contains(three.getPosition()), true);
+        assertEquals(competitors.contains(four.getPosition()), false);
+    }
+
+    //test the correct visibility through walls
+
+    @Test
+    public void testGetCardinalDirectionSquares() {
+        Match match = new Match();
+        Square square, temp;
+        ArrayList<Square> list;
+        for (int i = 0; i < ROWS; i++)
+            for (int j = 0; j < COLUMNS; j++) {
+                square = match.getBoard().getSquare(i, j);
+                if (square != null) {
+                    list = match.getBoard().getCardinalDirectionSquares(square, INF, 0, true);
+                    for (int r = 0; r < ROWS; r++)
+                        for (int c = 0; c < COLUMNS; c++) {
+                            temp = match.getBoard().getSquare(r, c);
+                            if (temp != null)
+                                assertEquals(list.contains(temp), r == i || c == j);
+                        }
+                }
+            }
+    }
+
+    //test the correct square's reachability
+
+    @Test
+    public void testGetReachableSquares() {
+        int maxMove = 6;
+        Match match = new Match();
+        Square square_one, square_two;
+        ArrayList<Square> list_one = new ArrayList<>();
+        ArrayList<Square> list_two = new ArrayList<>();
+        for (int i = 0; i < 1; i++)
+            for (int j = 0; j < 1; j++) {
+                square_one = match.getBoard().getSquare(i, j);
+                if (square_one != null) {
+                    match.getBoard().getReachableSquare(square_one, list_one, maxMove);
+                    for (int r = 2; r < 3; r++)
+                        for (int c = 1; c < 2; c++) {
+                            square_two = match.getBoard().getSquare(r, c);
+                            if (square_two != null) {
+                                match.getBoard().getReachableSquare(square_two, list_two, maxMove);
+                                assertEquals(list_two.contains(square_one), list_one.contains(square_two));
+                            }
+                        }
+                }
+            }
     }
 }
