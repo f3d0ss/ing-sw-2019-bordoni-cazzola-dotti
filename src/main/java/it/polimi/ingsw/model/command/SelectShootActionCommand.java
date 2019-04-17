ce1d1dbb7fb48b0e7085ee159a21fb8b65d08182
f-cazzola <federico.cazzola@mail.polimi.it>
2019-04-17 16:52:20 +0200

    Implement SelectShootAction

--- src/main/java/it/polimi/ingsw/model/command/SelectShootActionCommand.java
+++ src/main/java/it/polimi/ingsw/model/command/SelectShootActionCommand.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.model.command;
 
 import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.playerstate.ChoosingWeaponOptionState;
+import it.polimi.ingsw.model.playerstate.ChoosingWeaponState;
 import it.polimi.ingsw.model.playerstate.SelectedAggregateActionState;
 
 public class SelectShootActionCommand implements Command {
@@ -12,13 +14,19 @@ public class SelectShootActionCommand implements Command {
         this.currentState = currentState;
     }
 
+    /**
+     * This method change the player state to ChoosingWeaponState
+     */
     @Override
     public void execute() {
-
+        player.changeState(new ChoosingWeaponState(currentState.getSelectedAggregateAction()));
     }
 
+    /**
+     * This method restore the previous state
+     */
     @Override
     public void undo() {
-
+        player.changeState(currentState);
     }
 }
