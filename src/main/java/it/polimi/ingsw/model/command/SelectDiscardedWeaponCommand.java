6c5ead200c7f2fbee837ae9d2e814a44dddaecfe
f-cazzola <federico.cazzola@mail.polimi.it>
2019-04-17 16:07:05 +0200

    Implement SelectDiscardedWeaponCommand

--- src/main/java/it/polimi/ingsw/model/command/SelectDiscardedWeaponCommand.java
+++ src/main/java/it/polimi/ingsw/model/command/SelectDiscardedWeaponCommand.java
@@ -3,12 +3,14 @@ package it.polimi.ingsw.model.command;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Weapon;
 import it.polimi.ingsw.model.playerstate.DiscardingWeaponState;
+import it.polimi.ingsw.model.playerstate.PendingPaymentWeaponState;
 
 public class SelectDiscardedWeaponCommand implements Command {
 
     private Player player;
     private DiscardingWeaponState currentState;
     private Weapon weaponToDiscard;
+    private boolean wasLoaded;
 
     public SelectDiscardedWeaponCommand(Player player, DiscardingWeaponState currentState, Weapon weaponToDiscard) {
         this.player = player;
@@ -16,13 +18,27 @@ public class SelectDiscardedWeaponCommand implements Command {
         this.weaponToDiscard = weaponToDiscard;
     }
 
+    /**
+     * This method discard a weapon
+     */
     @Override
     public void execute() {
-
+        wasLoaded = weaponToDiscard.isLoaded();
+        weaponToDiscard.reload();
+        player.removeWeapon(weaponToDiscard);
+        currentState.getSpawn().addWeapon(weaponToDiscard);
+        player.changeState(new PendingPaymentWeaponState(currentState.getSelectedAggregateAction(), currentState.getSelectedWeapon()));
     }
 
+    /**
+     * This method restore the weapon discarded
+     */
     @Override
     public void undo() {
-
+        if (!wasLoaded)
+            weaponToDiscard.unload();
+        player.addWeapon(weaponToDiscard);
+        currentState.getSpawn().removeWeapon(weaponToDiscard);
+        player.changeState(currentState);
     }
 }
