2204c17683e8dabfa4f65e13eb0678770d0a3f2f
f-cazzola <federico.cazzola@mail.polimi.it>
2019-04-24 18:11:24 +0200

    Bug fix

--- src/main/java/it/polimi/ingsw/model/TargetingScope.java
+++ src/main/java/it/polimi/ingsw/model/TargetingScope.java
@@ -1,13 +1,7 @@
 package it.polimi.ingsw.model;
 
-import it.polimi.ingsw.model.command.Command;
-import it.polimi.ingsw.model.playerstate.TargetingPlayerState;
-
-import java.util.List;
-
 public class TargetingScope extends PowerUp {
     public static final int DAMAGE = 1;
-    private Player targetPlayer;
 
     public TargetingScope(String name, Color color) {
         super(name, color);
