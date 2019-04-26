99e823f1eb3414fc55d70b559e38de6138cf28c3
supernivem <48649510+supernivem@users.noreply.github.com>
2019-04-26 16:25:31 +0200

    Testing

--- src/test/java/it/polimi/ingsw/view/CliTest.java
+++ src/test/java/it/polimi/ingsw/view/CliTest.java
@@ -1,22 +1,42 @@
 package it.polimi.ingsw.view;
 
-import it.polimi.ingsw.model.Match;
-import it.polimi.ingsw.model.Player;
-import it.polimi.ingsw.model.PlayerId;
+import it.polimi.ingsw.model.*;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 public class CliTest {
 
     @Test
     public void displayBoard() {
-        Cli cli = new Cli();
         Match match = new Match();
-        for(PlayerId id : PlayerId.values()) {
-            Player player = new Player(match, id, null, match.getBoard().getSquare(0, 0));
-            match.addPlayer(player);
-            match.getBoard().getSquare(0, 0).addPlayer(player);
-        }
-        cli.setMatch(match);
-        cli.displayBoard();
+        PlayerId client = PlayerId.GREEN;
+        Player player = new Player(match, client, "Paolito", match.getBoard().getSpawn(Color.RED));
+        Player enemy1 = new Player(match, PlayerId.VIOLET, "FedeCazzola", match.getBoard().getSpawn(Color.RED));
+        Player enemy2 = new Player(match, PlayerId.GREY, "FranzDotti", match.getBoard().getSpawn(Color.RED));
+        match.addPlayer(player);
+        match.getBoard().getSpawn(Color.RED).addPlayer(player);
+        match.addPlayer(enemy1);
+        match.getBoard().getSpawn(Color.RED).addPlayer(enemy1);
+        match.addPlayer(enemy2);
+        match.getBoard().getSpawn(Color.RED).addPlayer(enemy2);
+        player.addAmmoTile(new AmmoTile(0, new HashMap<Color, Integer>() {{
+            put(Color.BLUE, 1);
+            put(Color.RED, 4);
+            put(Color.YELLOW, 1);
+        }}));
+        enemy1.addAmmoTile(new AmmoTile(0, new HashMap<Color, Integer>() {{
+            put(Color.BLUE, 1);
+            put(Color.RED, 0);
+            put(Color.YELLOW, 2);
+        }}));
+        player.addDamage(3, PlayerId.VIOLET);
+        player.addDamage(4, PlayerId.YELLOW);
+        player.addMarks(2, PlayerId.BLUE);
+        player.addWeapon(new Weapon());
+        enemy1.addWeapon(new Weapon());
+        match.restoreCards();
+        Cli cli = new Cli(match.getBoard().getHeight(), match.getBoard().getWidth(), match, client);
+        cli.update("Test");
     }
 }
