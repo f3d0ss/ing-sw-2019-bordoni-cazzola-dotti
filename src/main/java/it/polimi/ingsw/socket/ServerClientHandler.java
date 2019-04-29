061f7e1dbff275752f71251a45d1956d340420da
supernivem <48649510+supernivem@users.noreply.github.com>
2019-04-29 22:06:53 +0200

    First socket

---
+++ src/main/java/it/polimi/ingsw/socket/ServerClientHandler.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.socket;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerClientHandler implements Runnable {
+    private Socket socket;
+    public ServerClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+    public void run() {
+        try {
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+// Leggo e scrivo nella connessione finche' non ricevo "quit"
+            while (true) {
+                String line = in.nextLine();
+                if (line.equals("quit")) {
+                    break;
+                } else {
+                    out.println("Received: " + line);
+                    out.flush();
+                }
+            }
+// Chiudo gli stream e il socket
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
