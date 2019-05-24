1b04c74988871d9f22673c1bf30150bc9de9c12a
supernivem <48649510+supernivem@users.noreply.github.com>
2019-05-24 13:05:21 +0200

    Button

---
+++ src/main/java/it/polimi/ingsw/gui/App.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Login");
+        button = new Button("Click me");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
