02c086768b93734183818d07d95be401d79c5d2a
xX-1337-Xx <francesco3.dotti@mail.polimi.it>
2019-06-22 22:13:26 +0200

    Add post deserialization method for CommandMessage

--- src/main/java/it/polimi/ingsw/view/commandmessage/SimpleCommandMessage.java
+++ src/main/java/it/polimi/ingsw/view/commandmessage/SimpleCommandMessage.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.view.commandmessage;
 
 public class SimpleCommandMessage extends CommandMessage {
-    private String jsonType = getClass().getSimpleName();
 
     public SimpleCommandMessage(CommandType type) {
         super(type);
--- src/main/java/it/polimi/ingsw/view/commandmessage/WeaponModeCommandMessage.java
+++ src/main/java/it/polimi/ingsw/view/commandmessage/WeaponModeCommandMessage.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.view.commandmessage;
 
 public class WeaponModeCommandMessage extends CommandMessage {
     private String weaponMode;
-    private String jsonType = getClass().getSimpleName();
 
     public WeaponModeCommandMessage(CommandType type, String weaponMode) {
         super(type);
