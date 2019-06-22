41d032bb762f3d4825e1ae4bf925a72671135df5
xX-1337-Xx <48254456+xX-1337-Xx@users.noreply.github.com>
2019-06-22 22:31:07 +0200

    Add post deserialization method for CommandMessage (#57)

--- src/main/java/it/polimi/ingsw/view/commandmessage/ColorCommandMessage.java
+++ src/main/java/it/polimi/ingsw/view/commandmessage/ColorCommandMessage.java
@@ -4,7 +4,6 @@ import it.polimi.ingsw.model.Color;
 
 public class ColorCommandMessage extends CommandMessage {
     private Color color;
-    private String jsonType = getClass().getSimpleName();
 
     public ColorCommandMessage(CommandType type, Color color) {
         super(type);
